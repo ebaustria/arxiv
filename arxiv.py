c53b231b17b1077650a3730f0d91033c9fa96673
Eric Bush <ericbushmerp@gmail.com>
2021-06-08 16:33:23 +0200

    created two relations

--- arxiv.py
+++ arxiv.py
@@ -5,12 +5,11 @@ import numpy as np
 with open('assets/arxiv.json') as archive:
     data = json.load(archive)
 
-processedList = []
+relation1 = []
+relation2 = []
 for dictionary in data:
     numPages = '0'
     numFigures = '0'
-    numTables = '0'
-    numDefinitions = '0'
     comment = str()
     commentsList = str(dictionary["comments"]).split()
     categoriesList = str(dictionary["categories"]).split()
@@ -33,16 +32,6 @@ for dictionary in data:
         commentsList.remove(numFigures)
         commentsList.remove("figures")
 
-    if "tables" in commentsList:
-        numTables = commentsList[commentsList.index("tables") - 1]
-        commentsList.remove(numTables)
-        commentsList.remove("tables")
-
-    if "definitions" in commentsList:
-        numDefinitions = commentsList[commentsList.index("definitions") - 1]
-        commentsList.remove(numDefinitions)
-        commentsList.remove("definitions")
-
     index = 0
     while index < len(commentsList):
         commentsList[index] = commentsList[index].replace('\n', '')
@@ -64,25 +53,32 @@ for dictionary in data:
         category = catAndSubCat[0]
         subCategory = catAndSubCat[1]
 
-        newDict = {
+        smallRelationDictionary = {
+            "sub-category": subCategory,
+            "category": category
+        }
+
+        bigRelationDictionary = {
             "id": dictionary["id"].replace(" ", ""),
+            "sub-category": subCategory,
             "submitter": dictionary["submitter"].replace(" ", ""),
             "title": title,
             "pages": numPages,
             "figures": numFigures,
-            "tables": numTables,
-            "definitions": numDefinitions,
             "comment": comment,
-            "category": category,
-            "sub-category": subCategory,
             "abstract": abstract,
             "update_date": dictionary["update_date"],
             "author": dictionary["author"].replace(" ", "")
         }
 
-        processedList.append(newDict)
+        relation1.append(bigRelationDictionary)
+        if smallRelationDictionary not in relation2:
+            relation2.append(smallRelationDictionary)
 
-df = pd.DataFrame(processedList)
-np.savetxt('assets/table_values.txt', df, fmt='%s')
+df1 = pd.DataFrame(relation1)
+df2 = pd.DataFrame(relation2)
+np.savetxt('assets/article.txt', df1, fmt='%s')
+np.savetxt('assets/category.txt', df2, fmt='%s')
 
-print(df)
+print(df1)
+print(df2)
